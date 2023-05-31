73d0020185e82f8761fd8bc51a5db152cf649879
j1-y00n <byunjiyun@naver.com>
2023-06-01 00:32:52 +0900

    [시작]detail.html

--- tcat/views.py
+++ tcat/views.py
@@ -90,13 +90,15 @@ def create(request):
             tcat = tcat_form.save(commit=False)
             tcat.user = request.user
             tcat.save()
-            return redirect('tcat:detail', tcat.pk)
 
         if tcat.image:
             tcat.image_url = settings.MEDIA_URL + str(tcat.image)
             tcat.save()
-
             return redirect('tcat:detail', tcat.pk)
+        
+        else:
+            return redirect('tcat:detail', tcat.pk)
+            
     else:
         tcat_form = TcatForm()
 
@@ -105,6 +107,34 @@ def create(request):
     }
 
     return render(request, 'tcat/create.html', context)
+
+
+def delete(request, tcat_pk):
+    tcat = Tcat.objects.get(pk=tcat_pk)
+    if request.user == tcat.user:
+        tcat.delete()
+    return redirect('tcat:index')
+
+
+def update(request, tcat_pk):
+    tcat = Tcat.objects.get(pk=tcat_pk)
+    if request.user == tcat.user:
+        if request.method == "POST":
+            form = TcatForm(request.POST, request.FILES, instance=tcat)
+            if form.is_valid():
+                form.save()
+                return redirect('tcat:detail', tcat.pk)
+        else:
+            form = TcatForm(instance=tcat)
+    else:
+        return redirect('tcat:index')
+    
+    context = {
+        'tcat': tcat,
+        'form': 'form'
+    }
+
+    return render(request, 'tcat/update.html', context)
 
 
 def all_events(request):
