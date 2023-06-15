0a5a63bc845937f3d363091edee46e3e6e3bbc94
HyeonSeuk <seo5719@naver.com>
2023-06-15 10:18:20 +0900

    배포

--- pjt/settings.py
+++ pjt/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2u9iww_4r51rnnut+yteic_yc#%mnh!pc3utd*0vy+_kc&3hdt'
+import os
+from dotenv import load_dotenv
+
+# .env 파일을 읽어서 환경 변수로 설정
+load_dotenv()
+
+
+# os 모듈을 활용해서 환경 변수를 조회
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
